9fd45a2d7ff34acc4440b280c2d3227d11b1208c
majutharan <ananthamohan.majutharan@digiratina.com>
2019-06-19 10:11:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, '0.0.0.0');
